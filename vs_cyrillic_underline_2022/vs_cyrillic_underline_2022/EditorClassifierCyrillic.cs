45e781847646116b30d68423e3129c8e1af6d336
ilya-pishchalnikov <ilya_chaik@mail.ru>
2025-05-13 09:08:05 +0300

    Developed the extension for Visual Studio 2022

---
+++ vs_cyrillic_underline_2022/vs_cyrillic_underline_2022/EditorClassifierCyrillic.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace vs_cyrillic_underline_2022
+{
+    /// <summary>
+    /// Classifier that classifies all text as an instance of the "EditorClassifierCyrillic" classification type.
+    /// </summary>
+    internal class EditorClassifierCyrillic : IClassifier
+    {
+        /// <summary>
+        /// Classification type.
+        /// </summary>
+        private readonly IClassificationType classificationType;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EditorClassifierCyrillic"/> class.
+        /// </summary>
+        /// <param name="registry">Classification registry.</param>
+        internal EditorClassifierCyrillic(IClassificationTypeRegistryService registry)
+        {
+            this.classificationType = registry.GetClassificationType("EditorClassifierCyrillic1");
+        }
+
+        #region IClassifier
+
+#pragma warning disable 67
+
+        /// <summary>
+        /// An event that occurs when the classification of a span of text has changed.
+        /// </summary>
+        /// <remarks>
+        /// This event gets raised if a non-text change would affect the classification in some way,
+        /// for example typing /* would cause the classification to change in C# without directly
+        /// affecting the span.
+        /// </remarks>
+        public event EventHandler<ClassificationChangedEventArgs> ClassificationChanged;
+
+#pragma warning restore 67
+
+        /// <summary>
+        /// Gets all the <see cref="ClassificationSpan"/> objects that intersect with the given range of text.
+        /// </summary>
+        /// <remarks>
+        /// This method scans the given SnapshotSpan for potential matches for this classification.
+        /// In this instance, it classifies everything and returns each span as a new ClassificationSpan.
+        /// </remarks>
+        /// <param name="span">The span currently being classified.</param>
+        /// <returns>A list of ClassificationSpans that represent spans identified to be of this classification.</returns>
+        public IList<ClassificationSpan> GetClassificationSpans(SnapshotSpan span)
+        {
+            var classifications = new List<ClassificationSpan>();
+
+            // Пример: выделение всех слов "пример"
+            var text = span.GetText();
+
+            var matches = Regex.Matches(text, "[а-яА-Я]+");
+
+            foreach (Match match in matches)
+            {
+                var wordSpan = new SnapshotSpan(span.Snapshot, new SnapshotPoint(span.Snapshot, span.Start + match.Index), match.Length);
+                classifications.Add(new ClassificationSpan(wordSpan, classificationType));
+            }
+
+            return classifications;
+        }
+
+        #endregion
+    }
+}
---
+++ vs_cyrillic_underline_2022/vs_cyrillic_underline_2022/EditorClassifierCyrillicFormat.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+using System.Windows.Media;
+
+namespace vs_cyrillic_underline_2022
+{
+    /// <summary>
+    /// Defines an editor format for the EditorClassifierCyrillic type that has a purple background
+    /// and is underlined.
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "EditorClassifierCyrillic1")]
+    [Name("EditorClassifierCyrillic1")]
+    [UserVisible(true)] // This should be visible to the end user
+    [Order(Before = Priority.High)] // Set the priority to be after the default classifiers
+    internal sealed class EditorClassifierCyrillicFormat : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EditorClassifierCyrillicFormat"/> class.
+        /// </summary>
+        public EditorClassifierCyrillicFormat()
+        {
+            this.DisplayName = "EditorClassifierCyrillic1"; // Human readable version of the name
+            this.TextDecorations = System.Windows.TextDecorations.Underline; // Croсоdilе
+        }
+    }
+}
