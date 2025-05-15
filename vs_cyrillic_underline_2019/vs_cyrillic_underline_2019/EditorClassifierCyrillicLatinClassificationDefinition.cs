41b6ce087c02d133810c3f91d1f08ee275809ad6
ilya-pishchalnikov <ilya_chaik@mail.ru>
2025-05-15 07:05:15 +0300

    Feature that strongly highlights Cyrillic symbols within mixed Cyrillic-Latin words

---
+++ vs_cyrillic_underline_2019/vs_cyrillic_underline_2019/EditorClassifierCyrillicLatinClassificationDefinition.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace vs_cyrillic_underline_2019
+{
+    /// <summary>
+    /// Classification type definition export for EditorClassifierCyrillicLatin
+    /// </summary>
+    internal static class EditorClassifierCyrillicLatinClassificationDefinition
+    {
+        // This disables "The field is never used" compiler's warning. Justification: the field is used by MEF.
+#pragma warning disable 169
+
+        /// <summary>
+        /// Defines the "EditorClassifierCyrillicLatin" classification type.
+        /// </summary>
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("EditorClassifierCyrillicLatin")]
+        private static ClassificationTypeDefinition typeDefinition;
+
+#pragma warning restore 169
+    }
+}
---
+++ vs_cyrillic_underline_2022/vs_cyrillic_underline_2022/EditorClassifierCyrillicLatinFormat.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+using System.Windows.Media;
+
+namespace vs_cyrillic_underline_2022
+{
+    /// <summary>
+    /// Defines an editor format for the EditorClassifierCyrillicLatin type that has a purple background
+    /// and is underlined.
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "EditorClassifierCyrillicLatin")]
+    [Name("EditorClassifierCyrillicLatin")]
+    [UserVisible(true)] // This should be visible to the end user
+    [Order(Before = Priority.Default)] // Set the priority to be after the default classifiers
+    internal sealed class EditorClassifierCyrillicLatinFormat : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EditorClassifierCyrillicLatinFormat"/> class.
+        /// </summary>
+        public EditorClassifierCyrillicLatinFormat()
+        {
+            this.DisplayName = "EditorClassifierCyrillicLatin"; // Human readable version of the name
+            this.BackgroundColor = Colors.Yellow;
+            this.ForegroundColor = Colors.DarkRed;
+            this.TextDecorations = System.Windows.TextDecorations.Underline;
+        }
+    }
+}
