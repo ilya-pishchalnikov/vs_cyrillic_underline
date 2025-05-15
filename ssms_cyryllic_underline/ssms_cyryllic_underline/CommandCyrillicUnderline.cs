f92d878cdec4685f23cd33a48b6990574f5babf1
ilya-pishchalnikov <ilya_chaik@mail.ru>
2025-05-15 06:17:29 +0300

    Developed the extension for SSMS. It is activated only when the menu item is clicked and displays a MessageBox with mixed Cyrillic-Latin words

---
+++ ssms_cyryllic_underline/ssms_cyryllic_underline/CommandCyrillicUnderline.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace ssms_cyryllic_underline
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class CommandCyrillicUnderline
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("77480475-ca21-42e0-a3b4-354000c7c5b6");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandCyrillicUnderline"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private CommandCyrillicUnderline(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static CommandCyrillicUnderline Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in CommandCyrillicUnderline's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            Instance = new CommandCyrillicUnderline(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var dte2 = Util.GetDTE2();
+
+            IVsTextView textView = null;
+
+            if (!dte2.TryGetTextView(out textView))
+            {
+                return;
+            }
+
+
+            var result = textView.GetBuffer(out var textLines);
+
+            textLines.GetLastLineIndex(out var lastLine, out var lastIndex);
+
+            textView.GetTextStream(0, 0, lastLine, lastIndex, out var text);
+
+            var matches = Regex.Matches(text, "(@|@@|#|##|)[a-zA-Zа-яА-Я0-9_]+");
+
+            var mixedWords = new List<Match>();
+
+            foreach(Match match in matches)
+            {
+                if (Regex.IsMatch(match.Value, "[а-яА-Я]") && Regex.IsMatch(match.Value, "[a-zA-Z]"))
+                {
+                    mixedWords.Add(match);
+                }
+            }
+
+            var mixedWordsString = string.Empty;
+
+            for (int i = 0; i < mixedWords.Count; i++)
+            {
+                textView.GetLineAndColumn(mixedWords[i].Index, out var line, out var index);
+                mixedWordsString += $"{line + 1}:{index + 1} {mixedWords[i].Value}";
+                if (i < mixedWords.Count - 1)
+                {
+                    mixedWordsString += "\n";
+                }
+            }
+
+            //textView.
+
+
+            string message = mixedWordsString;
+            string title = "Identifiers with mixed Latin and Cyrillic symbols";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.package,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
---
+++ ssms_cyryllic_underline/ssms_cyryllic_underline/Util.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TextManager.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ssms_cyryllic_underline
+{
+    internal static class Util
+    {
+        public static EnvDTE80.DTE2 GetDTE2()
+        {
+            return (EnvDTE80.DTE2)Package.GetGlobalService(typeof(SDTE));
+        }
+
+        public static Boolean TryGetTextView(this EnvDTE80.DTE2 dte2, out IVsTextView textView)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            if (dte2.ActiveDocument == null)
+            {
+                textView = null;
+
+                return false;
+            }
+
+            var aDocIsOpen = VsShellUtilities.IsDocumentOpen(
+                provider: new ServiceProvider((Microsoft.VisualStudio.OLE.Interop.IServiceProvider)dte2),
+                fullPath: dte2.ActiveDocument.FullName,
+                logicalView: Guid.Empty,
+                hierarchy: out var uiHierarchy,
+                itemID: out var itemID,
+                windowFrame: out var windowFrame
+            );
+
+            if (!aDocIsOpen)
+            {
+                textView = null;
+
+                return false;
+            }
+
+            windowFrame.GetCodeWindow().Value.GetLastActiveView(out textView);
+           
+            return true;
+        }
+
+        /// <summary>
+        /// https://github.com/VsVim/VsVim/blob/d7b3e1a79a6d06cdae5e0334e09f9bbf5388e7df/Src/VsVimShared/Extensions.cs#L500
+        /// </summary>
+        /// <param name="vsWindowFrame"></param>
+        /// <returns></returns>
+        public static Result<IVsCodeWindow> GetCodeWindow(this IVsWindowFrame vsWindowFrame)
+        {
+            var iid = typeof(IVsCodeWindow).GUID;
+            var ptr = IntPtr.Zero;
+
+            try
+            {
+                var hr = vsWindowFrame.QueryViewInterface(ref iid, out ptr);
+
+                if (ErrorHandler.Failed(hr))
+                {
+                    return Result.CreateError(hr);
+                }
+
+                return Result.CreateSuccess((IVsCodeWindow)Marshal.GetObjectForIUnknown(ptr));
+            }
+            catch (Exception e)
+            {
+                // Venus will throw when querying for the code window
+                return Result.CreateError(e);
+            }
+            finally
+            {
+                if (ptr != IntPtr.Zero)
+                {
+                    Marshal.Release(ptr);
+                }
+            }
+        }
+    }
+}
